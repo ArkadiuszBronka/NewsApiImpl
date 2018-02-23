0a751ace0ffeec1341d38c3f8d7c581eb8f9698a
arkadiusz.bronka <arkadiusz.bronka@comarch.pl>
2018-02-23 08:53:39 +0100

    first refactoring

--- src/ab/impl/org/newsapi/core/Logic.java
+++ src/ab/impl/org/newsapi/core/Logic.java
@@ -11,6 +11,43 @@ import ab.impl.org.newsapi.data.TopHeadlines;
 
 public class Logic {
 
+	private String lang;
+	private String category;
+	private String pageSize;
+	private String page;
+	
+	public String getLang() {
+		return lang;
+	}
+
+	public void setLang(String lang) {
+		this.lang = lang;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getPageSize() {
+		return pageSize;
+	}
+
+	public void setPageSize(String pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	public String getPage() {
+		return page;
+	}
+
+	public void setPage(String page) {
+		this.page = page;
+	}
+	
 	public TopHeadlines readData() {
 		return readDataFromSource();
 	}
@@ -19,14 +56,30 @@ public class Logic {
 	{
 		// https://newsapi.org/v2/top-headlines?country=pl&apiKey=2c2274ddbc5340afb9dbe54ad525aabf&category=technology
 
+		System.out.println("lang=" + lang + " category=" + category + " pageSize=" + pageSize + " page=" + page);
+		
 		Client client = ClientBuilder.newClient();
 		WebTarget webTarget = client.target("https://newsapi.org");
-		WebTarget employeeWebTarget = webTarget.path("/v2/top-headlines");
-		employeeWebTarget = employeeWebTarget.queryParam("apiKey", "2c2274ddbc5340afb9dbe54ad525aabf");
-		employeeWebTarget = employeeWebTarget.queryParam("country", "pl");
-		employeeWebTarget = employeeWebTarget.queryParam("category", "technology");
-
-		Invocation.Builder invocationBuilder = employeeWebTarget.request(MediaType.APPLICATION_JSON);
+		webTarget = webTarget.path("/v2/top-headlines");
+		webTarget = webTarget.queryParam("apiKey", "2c2274ddbc5340afb9dbe54ad525aabf");
+		
+		if(lang != null) {
+			webTarget = webTarget.queryParam("country", lang);
+		}
+		
+		if(category != null) {
+			webTarget = webTarget.queryParam("category", category);
+		}
+		
+		if(pageSize != null) {
+			webTarget = webTarget.queryParam("pageSize", pageSize);
+		}
+		
+		if(page !=null) {
+			webTarget = webTarget.queryParam("page", page);
+		}
+		
+		Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
 
 		TopHeadlines topHeadlines = invocationBuilder.get(TopHeadlines.class);
 	
@@ -35,6 +88,9 @@ public class Logic {
 
 	public static void main(String[] args) {
 		Logic logic = new Logic();
+		logic.setLang("pl");
+		logic.setCategory("technology");
+
 		TopHeadlines topHeadlines = logic.readData();
 		
 		System.out.println("Status:" + topHeadlines.getStatus() + ", errorCode:" + topHeadlines.getCode() + ", message:" + topHeadlines.getMessage() + " count:" + topHeadlines.getTotalResults());
--- src/ab/impl/org/newsapi/web/rest/News.java
+++ src/ab/impl/org/newsapi/web/rest/News.java
@@ -2,33 +2,34 @@ package ab.impl.org.newsapi.web.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Invocation;
-import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import ab.impl.org.newsapi.core.Logic;
 import ab.impl.org.newsapi.data.TopHeadlines;
 
 @Path("news")
 public class News {
 
 	@GET
-	@Path("testRest")
-	//@Produces(MediaType.TEXT_PLAIN)
+	@Path("{lang}/{category}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response helloRestClient() {
-		Client client = ClientBuilder.newClient();
-		WebTarget webTarget = client.target("https://newsapi.org");
-		webTarget = webTarget.path("/v2/top-headlines");
-		webTarget = webTarget.queryParam("apiKey", "2c2274ddbc5340afb9dbe54ad525aabf");
-		webTarget = webTarget.queryParam("country", "pl");
-		webTarget = webTarget.queryParam("category", "technology");
-		Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+	public Response helloRestClient(
+			@PathParam("lang") String lang, 
+			@PathParam("category") String category,
+			@QueryParam("pageSize") String pageSize, 
+			@QueryParam("page") String page) {
 
-		TopHeadlines topHeadlines = invocationBuilder.get(TopHeadlines.class);
+		Logic logic = new Logic();
+		logic.setLang(lang);
+		logic.setCategory(category);
+		logic.setPageSize(pageSize);
+		logic.setPage(page);
+
+		TopHeadlines topHeadlines = logic.readData();
 
 		return Response.status(200).entity(topHeadlines).build();
 	}
